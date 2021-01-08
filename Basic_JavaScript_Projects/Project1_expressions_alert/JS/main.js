00e938091515bc0ab9ced0a4e4248d48bebe73cf
Mario Munoz <mario.zonum@gmail.com>
2021-01-07 21:21:34 -0500

    Create Variables

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,27 @@
-alert("Hello World!")
+window.alert("Hello World!")
+document.write('Hello, world!')
+
+var A = " This is a string. ";
+document.write(A);
+
+window.alert(A);
+
+var B = "As The Rock once said \"When you wake up in the morning, make sure that the devil curses out loud.\""
+
+document.write(B);
+
+document.write(A + B);
+
+var C = "Did you love her?" + " No! ";
+
+document.write(C);
+
+var D = "Mom", E = "Dad", F = "Brother";
+var D = D.fontcolor("red"), E = E.fontcolor("red");
+document.write(E);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3*3);
